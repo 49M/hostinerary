c8311429e82fea3ec8c769d63948378d72d9886a
49M <michal_buczek@yahoo.com>
2026-02-28 20:43:55 -0500

    start script

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,7 @@ import itineraryRouter from './routes/itinerary.js';
 import chatRouter from './routes/chat.js';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
